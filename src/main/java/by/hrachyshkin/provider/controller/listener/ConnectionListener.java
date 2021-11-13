75bfe6d6e9780496a5f374743e6514d358af7117
Ivan Hrachyshkin <ivanhrachyshkin@gmail.com>
2021-11-14 00:51:07 +0300

    add ConnectionListener

---
+++ src/main/java/by/hrachyshkin/provider/controller/listener/ConnectionListener.java
@@ -0,0 +1,21 @@
+package by.hrachyshkin.provider.controller.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+@WebListener
+public class ConnectionListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
